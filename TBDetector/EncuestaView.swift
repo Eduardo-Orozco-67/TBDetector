5c5f457ee58dbd7ffb50d49cbc0ad57cc86b6cee
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2023-04-26 12:39:50 -0600

    encuesta

--- TBDetector/EncuestaView.swift
+++ TBDetector/EncuestaView.swift
@@ -16,38 +16,96 @@ struct EncuestaView: View {
        @State private var resultado: String = ""
        
        var body: some View {
-           VStack {
+           VStack (spacing: 60) {
                Text("¿Tiene el niño alguno de estos síntomas?")
                    .font(.headline)
                    .padding()
-               
-               HStack {
-                   SymptomToggle(isOn: $tos, icon: "tose")
-                   SymptomToggle(isOn: $fiebre, icon: "fiebre")
-               }
-               
-               HStack {
-                   SymptomToggle(isOn: $sudoresNocturnos, icon: "sudores")
-                   SymptomToggle(isOn: $perdidaDePeso, icon: "peso")
-               }
-               
-               HStack {
-                   SymptomToggle(isOn: $fatiga, icon: "fatiga")
-               }
-               
-               Button(action: evaluar) {
-                   Text("Evaluar")
+               VStack (spacing: 60){
+                   HStack (spacing: 170){
+                       VStack {
+                           Image(systemName: "lungs.fill")
+                               .resizable()
+                               .aspectRatio(contentMode: .fit)
+                               .frame(width: 70, height: 70)
+                               .padding(.bottom, 10)
+                           
+                           Toggle(isOn: $tos) {
+                               Text("")
+                           }
+                           .labelsHidden()
+                       }
+                       .scaleEffect(1.5)
+                       VStack {
+                           Image(systemName: "thermometer.sun.fill")
+                               .resizable()
+                               .aspectRatio(contentMode: .fit)
+                               .frame(width: 70, height: 70)
+                               .padding(.bottom, 10)
+                           
+                           Toggle(isOn: $fiebre) {
+                               Text("")
+                           }
+                           .labelsHidden()
+                       }
+                       .scaleEffect(1.5)
+                   }
+                   
+                   HStack (spacing: 220){
+                       
+                       VStack {
+                           Image(systemName:  "bed.double.fill")
+                               .resizable()
+                               .aspectRatio(contentMode: .fit)
+                               .frame(width: 70, height: 70)
+                               .padding(.bottom, 10)
+                           
+                           Toggle(isOn: $sudoresNocturnos) {
+                               Text("")
+                           }
+                           .labelsHidden()
+                       }
+                       .scaleEffect(1.5)
+                       VStack {
+                           Image(systemName: "scalemass.fill")
+                               .resizable()
+                               .aspectRatio(contentMode: .fit)
+                               .frame(width: 70, height: 70)
+                               .padding(.bottom, 10)
+                           
+                           Toggle(isOn: $perdidaDePeso) {
+                               Text("")
+                           }
+                           .labelsHidden()
+                       }
+                       .scaleEffect(1.5)
+                       VStack {
+                           Image(systemName: "zzz")
+                               .resizable()
+                               .aspectRatio(contentMode: .fit)
+                               .frame(width: 70, height: 70)
+                               .padding(.bottom, 10)
+                           
+                           Toggle(isOn: $fatiga) {
+                               Text("")
+                           }
+                           .labelsHidden()
+                       }
+                       .scaleEffect(1.5)
+                   }
+                   Button(action: evaluar) {
+                       Text("Evaluar")
+                           .font(.headline)
+                           .foregroundColor(.white)
+                           .padding()
+                           .frame(maxWidth: .infinity)
+                           .background(Color.blue)
+                           .cornerRadius(10)
+                   }
+                   
+                   Text(resultado)
                        .font(.headline)
-                       .foregroundColor(.white)
                        .padding()
-                       .frame(maxWidth: .infinity)
-                       .background(Color.blue)
-                       .cornerRadius(10)
                }
-               
-               Text(resultado)
-                   .font(.headline)
-                   .padding()
                
            }
            .navigationBarTitle("Formulario de detección de tuberculosis")
