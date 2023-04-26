12146b5a38932b685dbf0a400568f201f7559e88
Emilia <emiliazlosada@outlook.com>
2023-04-26 10:32:54 -0600

    Pantalla de carga

---
+++ TBDetector/Color.swift
@@ -0,0 +1,17 @@
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    public static let Celeste: Color = Color(UIColor(red: 213/255, green: 230/255, blue: 240/255,alpha: 1.0))
+    
+    public static let Rosa: Color = Color(UIColor(red: 253/255, green: 90/255, blue: 94/255,alpha: 1.0))
+    
+    public static let AzulMarino: Color = Color(UIColor(red: 4/255, green: 56/255, blue: 85/255,alpha: 1.0))
+    
+    public static let AguaMarina: Color = Color(UIColor(red: 16/255, green: 128/255, blue: 142/255,alpha: 1.0))
+    
+    public static let Rojo: Color = Color(UIColor(red: 233/255, green: 23/255, blue: 32/255,alpha: 1.0))
+}
+
